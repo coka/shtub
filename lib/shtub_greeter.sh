ad56f6af057dc83b5ebbd60938986d3e8493c06e
Bojan Čoka <metropolis.lights@gmail.com>
2015-08-09 00:47:22 +0200

    Implement basic shtubbin'.

--- lib/shtub_greeter.sh
+++ lib/shtub_greeter.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env sh
 
 greet() { printf "Hello, world!"; }
+
+bid_farewell()
+{
+  if [ "$(has_cake)" = "Yes." ]; then
+    printf "Yum!"
+  else
+    printf "Goodbye, cruel world!"
+  fi
+}
--- spec/shtub_greeter_spec.sh
+++ spec/shtub_greeter_spec.sh
@@ -1,15 +1,35 @@
 #!/usr/bin/env sh
-
 require "shtub_greeter.sh"
 
 describe "shtub greeter"
+
   describe "greet()"
+
     it "returns 'Hello, world!'"
       expect greet --to-eql "Hello, world!"
     end
 
     it "does not return 'Goodbye, cruel world!'"
       expect greet --not-to-eql "Goodbye, cruel world!"
+    end
+  end
+
+  describe "bid_farewell()"
+
+    describe "when it has cake"
+
+      it "returns 'Yum!'"
+        allow has_cake --to-return "Yes."
+        expect bid_farewell --to-eql "Yum!"
+      end
+    end
+
+    describe "when it doesn't have cake"
+
+      it "returns 'Goodbye, cruel world!'"
+        allow has_cake --to-return "No!"
+        expect bid_farewell --to-eql "Goodbye, cruel world!"
+      end
     end
   end
 end
