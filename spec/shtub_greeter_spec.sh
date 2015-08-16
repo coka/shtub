b182146354956ac74d8eb78046cdf04f2b251f50
Bojan Čoka <metropolis.lights@gmail.com>
2015-08-16 23:30:19 +0200

    Update shtub greeter example.

--- spec/shtub_greeter_spec.sh
+++ spec/shtub_greeter_spec.sh
@@ -6,11 +6,11 @@ describe "shtub greeter"
   describe "greet()"
 
     it "returns 'Hello, world!'"
-      expect greet --to-eql "Hello, world!"
+      expect "$(greet)" --to-eql "Hello, world!"
     end
 
     it "does not return 'Goodbye, cruel world!'"
-      expect greet --not-to-eql "Goodbye, cruel world!"
+      expect "$(greet)" --not-to-eql "Goodbye, cruel world!"
     end
   end
 
@@ -20,7 +20,7 @@ describe "shtub greeter"
 
       it "returns 'Yum!'"
         allow has_cake --to-return "Yes."
-        expect bid_farewell --to-eql "Yum!"
+        expect "$(bid_farewell)" --to-eql "Yum!"
       end
     end
 
@@ -28,7 +28,7 @@ describe "shtub greeter"
 
       it "returns 'Goodbye, cruel world!'"
         allow has_cake --to-return "No!"
-        expect bid_farewell --to-eql "Goodbye, cruel world!"
+        expect "$(bid_farewell)" --to-eql "Goodbye, cruel world!"
       end
     end
   end
